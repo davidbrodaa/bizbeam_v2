9686b957f00ec15db40de3acf31460acaf5f3bde
davidbrodaa <david.broda@gmail.com>
2025-10-10 21:15:15 -0500

    Updated index.js

--- src/layouts/transmissions/projects/index.js
+++ src/layouts/transmissions/projects/index.js
@@ -65,7 +65,6 @@ function AllProjects() {
     >
       <MenuItem onClick={close}>Monitor</MenuItem>
       <MenuItem onClick={close}>Edit</MenuItem>
-     // <MenuItem onClick={close}>Request Permision</MenuItem>
     </Menu>
   );
 
